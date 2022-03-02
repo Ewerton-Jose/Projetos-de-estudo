3452b570a043578176009f3bc2050ee67d0c663b
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2022-03-02 19:43:41 -0300

    Criando o primeiro mini projeto
    - criando a "Calculadora de troco"
    - Trinando DOM em js

---
+++ calculadora_De_troco/calculadora.js
@@ -0,0 +1,7 @@
+function varia() {
+    var corpo = document.body
+    var sv = document.getElementById('sv1');
+    var svt = sv.ariaValueMax;
+    corpo.style.backgroundColor = 'black'
+    console.log('corpo')
+};
