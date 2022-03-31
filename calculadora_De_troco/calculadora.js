a7074abe6b9fe714eb7c7d4f7e783bbaea877aa9
Ewerton J. Silva <76182422+Ewerton-Jose@users.noreply.github.com>
2022-03-31 20:25:16 -0300

    Arrays adicionado em JS, css adicionado
    - CSS adicionado
    - Arrays sendo incluidos
    -

--- calculadora_De_troco/calculadora.js
+++ calculadora_De_troco/calculadora.js
@@ -1,7 +1,15 @@
-function varia() {
-    var corpo = document.body
-    var sv = document.getElementById('sv1');
-    var svt = sv.ariaValueMax;
-    corpo.style.backgroundColor = 'black'
-    console.log('corpo')
-};
+var produtinhos = []
+
+function cp() {
+    var prod = String((document.getElementById('produtos')).value)
+    var d = document.getElementById('por')
+    produtinhos.push(prod)
+    d.innerText = `${produtinhos}`
+}
+
+function m() {
+    var d = document.getElementById('por')
+    var sv = Number((document.getElementById('sv1')).value) 
+    var vp = Number((document.getElementById('vp1')).value)
+    d.innerText = `${sv} - ${vp} = ${sv - vp}`
+     }
